8af568d9d2c5518f44670e5ddc5efb2dc4eca276
eliyas ch <eliyaschozhakkadofficial@gmail.com>
2024-01-08 12:28:47 +0530

    Model training completed

--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -44,3 +44,14 @@ Data Transformation related constant start with DATA_TRANSFORMATION_VAR_NAME
 DATA_TRANSFORMATION_DIR_NAME:str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str = "transformed_object"
+
+
+"""
+Model Trainer realated constant start with MODEL TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME:str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR:str = "trained_model"
+MODEL_TRAINER_TRAINDED_MODEL_NAME:str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE:float = 0.6
+MODEL_OVERFITTING_UNDERFITTING_THRES:float = 0.05
--- sensor/utils/main_utils.py
+++ sensor/utils/main_utils.py
@@ -63,4 +63,14 @@ def save_object(file_path:str, obj:object) -> None:
     except Exception as e:
         raise SensorException(e,sys)
     
+def load_object(file_path:str) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file path:{file_path} not found")
+        with open(file_path,"rb") as file_obj:
+            dill.load(file_obj)
+            return dill
+    except Exception as e:
+        raise SensorException(e,sys)
+    
 
